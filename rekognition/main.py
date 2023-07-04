31d9440ccf909695fc81200d724b1d2400aa574f
Alexander <alfrvo@gmail.com>
2023-07-04 09:41:15 +0200

    first commit

---
+++ rekognition/main.py
@@ -0,0 +1,43 @@
+import sys
+import boto3
+
+def detect_labels(photo, bucket):
+
+     session = boto3.Session(profile_name='account2')
+     client = session.client('rekognition')
+
+     response = client.detect_labels(Image={'S3Object':{'Bucket':bucket,'Name':photo}},
+     MaxLabels=10,
+     )
+
+     print('Detected labels for ' + photo)
+     print()
+     for label in response['Labels']:
+         print("Label: " + label['Name'])
+         print("Confidence: " + str(label['Confidence']))
+         print("Instances:")
+
+         print("Parents:")
+         for parent in label['Parents']:
+            print(" " + parent['Name'])
+
+         print("Aliases:")
+         for alias in label['Aliases']:
+             print(" " + alias['Name'])
+
+             print("Categories:")
+         for category in label['Categories']:
+             print(" " + category['Name'])
+             print("----------")
+             print()
+
+     return len(response['Labels'])
+
+def main():
+    bucket = sys.argv[1]
+    photo = sys.argv[2]
+    label_count = detect_labels(photo, bucket)
+    print("Labels detected: " + str(label_count))
+
+if __name__ == "__main__":
+    main()
