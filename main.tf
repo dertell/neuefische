0325a1818e2801ae8e345f608009e02b942a5994
Alexander <alfrvo@gmail.com>
2023-07-05 16:00:27 +0200

    third terra commit

--- main.tf
+++ main.tf
@@ -6,6 +6,7 @@ provider "aws" {
 resource "aws_instance" "deham6ec2"{
     ami = "ami-02d8bad0a1da4b6fd"
     instance_type = "t2.micro"
+    key_name = "terraform-key"
     vpc_security_group_ids = ["sg-075fed123413fd78d"]
     subnet_id = "subnet-035fe92ae49c8a24b"
     tags = {
