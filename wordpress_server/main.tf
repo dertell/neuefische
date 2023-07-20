be8d4df8f9683a976d729de167353d00887db129
Alexander <alfrvo@gmail.com>
2023-07-20 13:02:23 +0200

    using variables in script

--- wordpress_server/main.tf
+++ wordpress_server/main.tf
@@ -63,16 +63,19 @@
             Name = "my_vpc_public_route"
         }
     }
+
     resource "aws_route_table_association" "public_subnet_association" {
         route_table_id              = aws_route_table.public_route.id
         subnet_id                   = aws_subnet.public_subnet_1.id
         depends_on                  = [aws_route_table.public_route, aws_subnet.public_subnet_1]
     }
+
     resource "aws_route_table_association" "public_subnet_2_association" {
         route_table_id              = aws_route_table.public_route.id
         subnet_id                   = aws_subnet.public_subnet_2.id
         depends_on                  = [aws_route_table.public_route, aws_subnet.public_subnet_2]
     }
+
     resource "aws_security_group" "my_vpc_sg_allow_http"{
         vpc_id                      = aws_vpc.my_vpc.id
         name                        = "my_vpc_allow_http"
@@ -80,7 +83,7 @@
             Name = "my_vpc_sg_allow_http"
         }
     }
-    resource "aws_security_group_rule" "devVPC_https_egress_access"{
+    resource "aws_security_group_rule" "http_ingress_access"{
         from_port                   = 80
         protocol                    = "tcp"
         security_group_id           = aws_security_group.my_vpc_sg_allow_http.id
@@ -88,8 +91,7 @@
         type                        = "ingress"
         cidr_blocks                 = [var.cidr_block]
     }
-
-    resource "aws_security_group_rule" "ssh_ingress_access"{
+    resource "aws_security_group_rule" "https_egress_access"{
         from_port                   = 443
         protocol                    = "tcp"
         security_group_id           = aws_security_group.my_vpc_sg_allow_http.id
@@ -106,14 +108,23 @@
             values = ["al2023-ami-2023*"]
             }
     }
+    locals {
+            DB      ="mydb"
+            User    ="alex"
+            PW      ="password123"
+    }
     resource "aws_instance" "webserver" {
         ami                         = data.aws_ami.amzLinux.id
         instance_type               = "t2.micro"
         key_name                    = "vockey"
         vpc_security_group_ids      = [aws_security_group.my_vpc_sg_allow_http.id]
         subnet_id                   = aws_subnet.public_subnet_1.id
-        user_data                   = "${file("user-data.sh")}"
+        user_data                   = templatefile("user-data.sh",{
+            DB      = local.DB
+            User    = local.User
+            PW      = local.PW
+        })
         tags = {
-            Name = "webserver"
+            Name    = "webserver"
         }
     }
--- wordpress_server/user-data.sh
+++ wordpress_server/user-data.sh
@@ -7,9 +7,9 @@ sudo systemctl start httpd
 sudo systemctl enable mariadb
 sudo systemctl start mariadb
 
-sudo mysql -e "CREATE DATABASE mydb /*\!40100 DEFAULT CHARACTER SET utf8 */;"
-sudo mysql -e "CREATE USER alex@localhost IDENTIFIED BY 'password123';"
-sudo mysql -e "GRANT ALL PRIVILEGES ON mydb.* TO 'alex'@'localhost';"
+sudo mysql -e "CREATE DATABASE ${DB} /*\!40100 DEFAULT CHARACTER SET utf8 */;"
+sudo mysql -e "CREATE USER ${User}@localhost IDENTIFIED BY '${PW}';"
+sudo mysql -e "GRANT ALL PRIVILEGES ON ${DB}.* TO '${User}'@'localhost';"
 sudo mysql -e "FLUSH PRIVILEGES;"
 
 curl -LO https://wordpress.org/latest.zip
@@ -19,8 +19,8 @@ sudo unzip latest.zip
 sudo mv -f wordpress/* ./
 
 sudo cp wp-config-sample.php wp-config.php 
-sudo sed -i 's/database_name_here/mydb/' wp-config.php 
-sudo sed -i 's/username_here/alex/' wp-config.php 
-sudo sed -i 's/password_here/password123/' wp-config.php
+sudo sed -i "s/database_name_here/${DB}/" wp-config.php
+sudo sed -i "s/username_here/${User}/" wp-config.php
+sudo sed -i "s/password_here/${PW}/" wp-config.php
 
 sudo service httpd restart
