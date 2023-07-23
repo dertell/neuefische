c101a57ccdd00b510a59f1c77e145917b6c4bb52
Alexander <alfrvo@gmail.com>
2023-07-23 18:51:57 +0200

    added a target group rule

--- wordpress_server/loadbalancer.tf
+++ wordpress_server/loadbalancer.tf
@@ -5,13 +5,12 @@ resource "aws_lb" "load-balancer" {
   security_groups                   = [aws_security_group.alb-sg.id]
   subnets                           = [aws_subnet.public_subnet_1.id, aws_subnet.public_subnet_2.id]
   enable_deletion_protection        = false
-  enable_http2                      = true
     tags = {
         Environment                 = "production"
   }
 }
 resource "aws_lb_target_group" "target-group" {
-  name                              = "webserver-tg"
+  name                              = "CPUtest-tg"
   port                              = 80
   protocol                          = "HTTP"
   vpc_id                            = aws_vpc.my_vpc.id
@@ -24,4 +23,29 @@ resource "aws_lb_listener" "listener" {
     type                            = "forward"
     target_group_arn                = aws_lb_target_group.target-group.arn
   }
+}
+
+resource "aws_lb_listener_rule" "wordpress-rule" {
+  listener_arn = aws_lb_listener.listener.arn
+  priority     = 100
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.target-group-wordpress.arn
+  }
+  condition {
+    path_pattern {
+      values = ["/wordpress/"]
+    }
+  }
+}
+resource "aws_lb_target_group" "target-group-wordpress" {
+  name                              = "webserver-tg"
+  port                              = 80
+  protocol                          = "HTTP"
+  vpc_id                            = aws_vpc.my_vpc.id
+}
+resource "aws_lb_target_group_attachment" "wordpress" {
+  target_group_arn = aws_lb_target_group.target-group-wordpress.arn
+  target_id        = aws_instance.webserver.id
+  port             = 80
 }
